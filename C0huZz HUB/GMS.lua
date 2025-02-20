5e559b766b523be9fef02cc390ce5aa2e88443bf
C0huZz993 <c0huzz.chz@gmail.com>
2025-02-20 22:00:45 +0500

    Delete C0huZz HUB/GMS.lua

--- C0huZz HUB/GMS.lua
+++
@@ -1,5 +0,0 @@
-If game.PlaceId == 4924922222 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/C0huZz993/ScriptsBackupRBX/refs/heads/main/C0huZz%20HUB/BRKRP.lua"))()
-else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/C0huZz993/ScriptsBackupRBX/refs/heads/main/C0huZz%20HUB/Universal.lua")()
-end
