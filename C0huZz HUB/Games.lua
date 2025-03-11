f772564c2397649f103702f31e40225ba3887464
C0huZz993 <c0huzz.chz@gmail.com>
2025-03-11 15:02:25 +0500

    Update Games.lua

--- C0huZz HUB/Games.lua
+++ C0huZz HUB/Games.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 4924922222 then
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/C0huZz993/ScriptsBackupRBX/refs/heads/main/C0huZz%20HUB/BRKRP.lua'))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/C0huZz993/ScriptsBackupRBX/refs/heads/main/C0huZz%20HUB/BRKRP.lua"))()
 else
-  loadstring(game:HttpGet('https://raw.githubusercontent.com/C0huZz993/ScriptsBackupRBX/refs/heads/main/C0huZz%20HUB/Universal.lua))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/C0huZz993/ScriptsBackupRBX/refs/heads/main/C0huZz%20HUB/Universal.lua"))()
 end
