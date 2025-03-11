96159a04ebdb1516e5e8b64fef6b3000e572f968
C0huZz993 <c0huzz.chz@gmail.com>
2025-03-11 17:26:34 +0500

    Create Loader.lua

---
+++ C0huZz HUB/Loader.lua
@@ -0,0 +1,10 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "C0huZz HUB";
+    Text = "Loading... wait 5 sec";
+    Duration = 5;
+    Button1 = "OK";
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/C0huZz993/ScriptsBackupRBX/refs/heads/main/C0huZz%20HUB/Games.lua"))()
